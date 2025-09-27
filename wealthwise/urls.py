d50d452bab0df8e5050777cf835f1f66be5b456c
Wilson Rustiandy <wilrustiandy@gmail.com>
2025-09-27 16:18:24 +0800

    Init core

--- wealthwise/urls.py
+++ wealthwise/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path as url
+
+from core import views as core_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', core_views.dashboard , name=""),
+    url(r'^dashboard$', core_views.dashboard , name="dashboard"),
 ]
