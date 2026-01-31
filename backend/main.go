3fbc5f2835990c1319357cec46c9ddb75ccecd99
Wilson Rustiandy <wilrustiandy@gmail.com>
2026-01-31 23:06:09 +0800

    Apply server graceful shutdown

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/wilrustiandy/wealthwise/backend/internal/logger"
 )
@@ -11,9 +17,31 @@ func main() {
 
 	host := "localhost:8080"
 
-	log.Info("WealthWise Backend starting on %s", host)
-	if err := http.ListenAndServe(host, nil); err != nil {
-		log.Fatal("Server failed to start: %v", err)
+	server := &http.Server{
+		Addr:    host,
+		Handler: nil,
 	}
-	log.Info("WealthWise Backend started!!")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	log.Info("WealthWise Backend starting on %s", host)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal("Server failed to start: %v", err)
+		}
+	}()
+
+	sig := <-shutdown
+	log.Warn("Received signal: %v. Shutting down gracefully...", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error("Graceful shutdown failed: %v", err)
+		server.Close()
+	}
+
+	log.Info("WealthWise Backend stopped!!")
 }
