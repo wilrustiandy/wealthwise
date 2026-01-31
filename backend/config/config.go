d5e29b584bfb5ab860acecb2f98ab70113bf3dcc
Wilson Rustiandy <wilrustiandy@gmail.com>
2026-01-31 23:21:43 +0800

    Initial configuration

---
+++ backend/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"errors"
+	"os"
+
+	yaml "gopkg.in/yaml.v3"
+)
+
+type LogConfig struct {
+	Level string `yaml:"level"`
+}
+
+type Config struct {
+	Address string    `yaml:"addr"`
+	Port    string    `yaml:"port"`
+	Env     string    `yaml:"env"`
+	Log     LogConfig `yaml:"log"`
+}
+
+const configFileName = "config.yaml"
+
+func Load() (*Config, error) {
+	var cfg Config
+
+	if err := yaml.Unmarshal([]byte(defaultConfig), &cfg); err != nil {
+		return nil, err
+	}
+
+	if _, err := os.Stat(configFileName); errors.Is(err, os.ErrNotExist) {
+		return &cfg, nil
+	}
+
+	fileData, err := os.ReadFile(configFileName)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(fileData, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
---
+++ backend/config/default.go
@@ -0,0 +1,9 @@
+package config
+
+const defaultConfig = `
+addr: localhost
+port: 8080
+env: dev
+log:
+    level: DEBUG
+`
